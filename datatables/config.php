097b2b5f72946057156e707cb2a225954efa8f32
Rene Vis <rpmvis@gmail.com>
2016-06-15 10:20:38 +0200

    datatables/config.php

--- datatables/config.php
+++ datatables/config.php
@@ -5,29 +5,20 @@ if (!defined('DATATABLES')) exit(); // Ensure being used in DataTables env.
 // Enable error reporting for debugging (remove for production)
 error_reporting(E_ALL);
 ini_set('display_startup_errors', 1);
+ini_set('display_startup_errors', 1);
 ini_set('display_errors', '1');
 
-// *** ReneVis: set database connection here
+// *** ReneVis: set database connection for datatables here
 /* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
  * Database user / pass
  */
 
-//$sql_details = array(
-//	"type" => "Mysql",  // Database type: "Mysql", "Postgres", "Sqlite" or "Sqlserver"
-//	"user" => env('DB_USERNAME', ''),
-//	"pass" => env('DB_PASSWORD', ''),
-//	"host" => env('DB_HOST', 'localhost'),
-//	"port" => env('DB_PORT', '3306'),
-//	"db"   => env('DB_DATABASE', ''),
-//	"dsn"  => "charset=utf8"
-//);
-//
 $sql_details = array(
 	"type" => "Mysql",  // Database type: "Mysql", "Postgres", "Sqlite" or "Sqlserver"
-	"user" => 'root',
-	"pass" => 'guppie',
-	"host" => 'localhost',
-	"port" => '3306',
-	"db"   => 'datatables_edit',
+	"user" => env('OPENSHIFT_MYSQL_DB_USERNAME', 'root'),
+	"pass" => env('OPENSHIFT_MYSQL_DB_PASSWORD', 'guppie'),
+	"host" => env('OPENSHIFT_MYSQL_DB_HOST'    , 'localhost'),
+	"port" => env('OPENSHIFT_MYSQL_DB_PORT'    , 3306),
+	"db"   => env('OPENSHIFT_APP_NAME'         , 'datatables_edit'),
 	"dsn"  => "charset=utf8"
 );
