e13c325f475b92a0641fd95a00cec31d81e5a0ed
Rene Vis <rpmvis@gmail.com>
2016-07-01 19:58:49 +0200

    modified:   app/Helpers/ControllerHelper.php

--- app/Helpers/ControllerHelper.php
+++ app/Helpers/ControllerHelper.php
@@ -11,15 +11,15 @@ class ControllerHelper
 {
     static function fix_OPENSHIFT_draw_parameter($request_input){
         /* OPENSHIFT hosting platform is returning the draw request parameter
-           as "\\draw", for some obscure reason
-           this function repairs this anomaly.
+           as "\\draw", for some obscure reason.
+           This function repairs this anomaly.
         */
         $key1 =  "\\draw";
         $key2 =  "draw";
         if (array_key_exists($key1, $request_input)){
             $value = $request_input[$key1];
-            $input[$key2] = $value;
-            unset($input[$key1]);
+            $request_input[$key2] = $value;
+            unset($request_input[$key1]);
         }
 
         return $request_input;
